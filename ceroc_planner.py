ea85828f75132c91848a976b53d1484c897c6dc3
Sharon Colson <sharon.colson@gmail.com>
2023-11-20 13:32:51 -0600

    add functionality to update last date of communication

--- ceroc_planner.py
+++ ceroc_planner.py
@@ -1,12 +1,13 @@
 # Import Dependencies
 import json
+from datetime import datetime
 from pprint import pprint
 
 # Contact class
 # Input: FirstName, LastName, UID, EmailAddress, Dept, Title,
 #           Phone_number, Building, POBox)
 class Contact:
-    def __init__(self, FirstName: str, LastName: str, UID: int, EmailAddress: str, Dept: str, Title: str, Phone: str, Building: str, POBox: int):
+    def __init__(self, FirstName: str, LastName: str, UID: int, EmailAddress: str, Dept: str, Title: str, Phone: str, Building: str, POBox: int, LastCommunicationDate=None):
         self.FirstName = FirstName
         self.LastName = LastName
         self.UID = UID
@@ -16,6 +17,7 @@ class Contact:
         self.Phone = Phone
         self.Building = Building
         self.POBox = POBox
+        self.LastCommunicationDate = LastCommunicationDate
 
 # Event class
 # Input: Name, UID, Date, StartTime, Location, Duration
@@ -50,12 +52,31 @@ def create_objects(json_data, object_class):
         objects.append(obj)
     return objects
 
+# Function view_objects
+# Input: objects array from the create_objects function and string for header
+# Output: the objects created from the json file
 def view_objects(objects, object_type):
     print(f"{object_type}s:")
     for object in objects:
         pprint(vars(object))
 
+def update_communication_date(contact_objects):
+    email_to_lookup = input("Enter the email of the contact you wish to update: ")
+    found_contact = None
 
+    for contact in contact_objects:
+        if contact.EmailAddress == email_to_lookup:
+            found_contact = contact
+            break
+    if found_contact: 
+        last_communication_date = found_contact.LastCommunicationDate
+        print(f"Last Date of Communication: {last_communication_date}")
+
+        new_communication_date = input("Enter new last communication date (YYYY-MM-DD): ")
+        found_contact.LastCommunicationDate = new_communication_date
+        print(f"Updated Last Communication Date: {found_contact.LastCommunicationDate}")
+    else:
+        print(f"Contact with email {email_to_lookup} not found.")
 
 def main():
     # Read data from contacts.json using the read_file function
@@ -70,7 +91,10 @@ def main():
     # Call the create_objects function on the events data 
     events_objects = create_objects(events_data['university_events'], Event)
 
+    # While statement to loop over menu until user is finished.
     while True:
+
+        # Print menu for user
         menu = """\nMenu:
 1. View Contacts
 2. View Events
@@ -80,26 +104,39 @@ def main():
 
 """
         print(menu)
-        
+
+        # Take in user's choice
         choice = input("Enter your choice (1-5): ")
 
+        # Converts to integer
         try:
             choice = int(choice)
         except ValueError:
             print("Invalid input. Please enter a number 1-5.")
             continue
         
+        # Print contact list
         if choice == 1:
             view_objects(contact_objects, "Contact")
+
+        # Print event list
         elif choice == 2:
             view_objects(events_objects, "Event")
+
+        # Update date of last contact
         elif choice == 3:
-            print("Fix me.")
+            update_communication_date(contact_objects)
+
+        # Add action items
         elif choice == 4:
             print("Fix me too.")
+
+        # Exit
         elif choice == 5:
             print("Exiting program. Goodbye!")
             break
+
+        # Error check
         else:
             print("Invalid choice. Please choose a number between 1 and 5.")
 
